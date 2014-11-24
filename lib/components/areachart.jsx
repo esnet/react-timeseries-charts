f0ba511fc41195f48edee2778b47fefd80686730
Antony Courtney <antony.courtney@gmail.com>
2014-11-24 13:17:38 -0800

    rename interpolation to interpolate for consistency with LineChart, d3

--- lib/components/areachart.jsx
+++ lib/components/areachart.jsx
@@ -20,7 +20,7 @@ function scaleAsString(scale) {
  */
 function getOptions(props) {
     // We use _.pick here so that keys not present in props will not be present in options
-    return _.pick(props, 'interpolation' /* , ... */ );
+    return _.pick(props, 'interpolate' /* , ... */ );
 }
 
 var AreaChart = React.createClass({
@@ -50,19 +50,19 @@ var AreaChart = React.createClass({
         // y0 here is the base, y1 is the top of the graph, and x is x.
         //
 
-        var interpolation = (options && _.has(options, "interpolation")) ? options.interpolation : "step-after";
+        var interpolate = (options && _.has(options, "interpolate")) ? options.interpolate : "step-after";
 
         var upArea = d3.svg.area()
             .x(function(d)  { return timeScale(d.date); })
             .y0(function(d) { return yScale(d.y0); })
             .y1(function(d) { return yScale(d.y0 + d.value); })
-            .interpolate(interpolation);
+            .interpolate(interpolate);
 
         var downArea = d3.svg.area()
             .x(function(d)  { return timeScale(d.date); })
             .y0(function(d) { return yScale(d.y0); })
             .y1(function(d) { return yScale(d.y0 - d.value); })
-            .interpolate(interpolation);
+            .interpolate(interpolate);
 
         //
         // Our D3 stack. When this is evoked with data (an array of layers) it builds up
