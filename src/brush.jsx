f47335c8f77fa407324922ffb5bdc97cea515ab2
Peter Murphy <pmurphy@es.net>
2016-06-05 18:27:51 -0700

    Started rewriting the brush code without d3. So far:  - you can grab the brush and drag, or grab the sides of it and resize  - Interacts with the rest of the page. i.e. you can resize it from outside the page, or use the brush to control something outside the page.

--- src/brush.jsx
+++ src/brush.jsx
@@ -1,5 +1,5 @@
 /**
- *  Copyright (c) 2015, The Regents of the University of California,
+ *  Copyright (c) 2016, The Regents of the University of California,
  *  through Lawrence Berkeley National Laboratory (subject to receipt
  *  of any required approvals from the U.S. Dept. of Energy).
  *  All rights reserved.
@@ -9,72 +9,235 @@
  */
 
 import React from "react";
-import ReactDOM from "react-dom";
-import { brushX, brushSelection } from "d3-brush";
-import { select, event } from "d3-selection";
 import { TimeRange } from "pondjs";
 
-function scaleAsString(scale) {
-    return `${scale.domain().toString()}-${scale.range().toString()}`;
-}
-
+/**
+ * Renders a band with extents defined by the supplied TimeRange.
+ */
 export default React.createClass({
 
-    displayName: "Brush",
+    displayName: "TimeRangeMarker",
 
-    componentWillMount() {
-        this.brush = brushX()
-            .on("brush", this.handleBrushed);
+    propTypes: {
+        timeRange: React.PropTypes.instanceOf(TimeRange).isRequired,
+        style: React.PropTypes.object
     },
 
-    handleBrushed() {
-        const [x1, x2] = event.selection;
+    getDefaultProps() {
+        return {
+            spacing: 1,
+            offset: 0,
+            style: {
+                fill: "rgba(70, 130, 180, 0.25)"
+            }
+        };
+    },
 
-        const d1 = this.props.timeScale.invert(x1);
-        const d2 = this.props.timeScale.invert(x2);
-
-        this.currentBegin = d1.getTime();
-        this.currentEnd = d2.getTime();
-
-        if (this.props.onTimeRangeChanged) {
-            this.props.onTimeRangeChanged(new TimeRange(d1, d2));
+    getInitialState() {
+        return {
+            isBrushing: false
         }
     },
 
-    renderBrush(timeScale, timeRange) {
-        if (timeRange.begin().getTime() !== this.currentBegin ||
-            timeRange.end().getTime() !== this.currentEnd) {
+    handleBrushMouseDown(e) {
+        e.preventDefault();
 
-            const x1 = this.props.timeScale(timeRange.begin());
-            const x2 = this.props.timeScale(timeRange.end());
+        const x = e.pageX;
+        const y = e.pageY;
+        const xy0 = [Math.round(x), Math.round(y)];
+        const begin = this.props.timeRange.begin().getTime();
+        const end = this.props.timeRange.end().getTime();
 
-            select(this.refs.group).selectAll("*").remove();
-            select(this.refs.group)
-                .append("g")
-                    .attr("class", "brush")
-                    .call(this.brush)
-                    .call(this.brush.move, [x1, x2]);
+        this.setState({
+            isBrushing: true,
+            brushingInitializationSite: "brush",
+            initialBrushBeginTime: begin,
+            initialBrushEndTime: end,
+            initialBrushXYPosition: xy0
+        });
+    },
+
+    handleLeftHandleMouseDown(e) {
+        e.preventDefault();
+
+        const x = e.pageX;
+        const y = e.pageY;
+        const xy0 = [Math.round(x), Math.round(y)];
+        const begin = this.props.timeRange.begin().getTime();
+        const end = this.props.timeRange.end().getTime();
+
+        this.setState({
+            isBrushing: true,
+            brushingInitializationSite: "handle-left",
+            initialBrushBeginTime: begin,
+            initialBrushEndTime: end,
+            initialBrushXYPosition: xy0
+        });
+    },
+
+    handleRightHandleMouseDown(e) {
+        e.preventDefault();
+
+        const x = e.pageX;
+        const y = e.pageY;
+        const xy0 = [Math.round(x), Math.round(y)];
+        const begin = this.props.timeRange.begin().getTime();
+        const end = this.props.timeRange.end().getTime();
+
+        this.setState({
+            isBrushing: true,
+            brushingInitializationSite: "handle-right",
+            initialBrushBeginTime: begin,
+            initialBrushEndTime: end,
+            initialBrushXYPosition: xy0
+        });
+    },
+
+    handleMouseUp(e) {
+        e.preventDefault();
+        this.setState({
+            isBrushing: false,
+            brushingInitializationSite: null,
+            initialBrushBeginTime: null,
+            initialBrushEndTime: null,
+            initialBrushXYPosition: null
+        });
+    },
+
+    handleMouseMove(e) {
+        e.preventDefault();
+
+        const x = e.pageX;
+        const y = e.pageY;
+        const xy = [Math.round(x), Math.round(y)];
+
+        if (this.state.isBrushing) {
+            const xy0 = this.state.initialBrushXYPosition;
+
+            const timeOffset =
+                this.props.timeScale.invert(xy0[0]).getTime() -
+                this.props.timeScale.invert(xy[0]).getTime();
+
+            let newBegin =
+                this.state.brushingInitializationSite === "brush" ||
+                this.state.brushingInitializationSite === "handle-left" ?
+                    parseInt(this.state.initialBrushBeginTime - timeOffset, 10) :
+                    this.state.initialBrushBeginTime;
+
+            let newEnd =
+                this.state.brushingInitializationSite === "brush" ||
+                this.state.brushingInitializationSite === "handle-right" ?
+                    parseInt(this.state.initialBrushEndTime - timeOffset, 10) :
+                    this.state.initialBrushEndTime;
+
+            if (this.props.onTimeRangeChanged) {
+                this.props.onTimeRangeChanged(new TimeRange(newBegin, newEnd));
+            }
         }
     },
 
-    componentDidMount() {
-        this.renderBrush(this.props.timeScale, this.props.timeRange);
+    renderOverlay() {
+        const { width, height } = this.props;
+        const overlayStyle = {fill: "red", opacity: 0.1};
+        return (
+            <rect
+                x={0} y={0}
+                width={width} height={height}
+                style={overlayStyle}
+                clipPath={this.props.clipPathURL} />
+        );
     },
 
-    componentWillReceiveProps(nextProps) {
-        const timeScale = nextProps.timeScale;
-        const timeRange = nextProps.timeRange;
-        if (scaleAsString(this.props.timeScale) !== scaleAsString(timeScale) ||
-            this.props.timeRange !== timeRange) {
-            this.renderBrush(timeScale, timeRange);
+    renderBrush() {
+        const { timeRange, timeScale, width, height, style } = this.props;
+
+        // Viewport bounds
+        const viewBeginTime = timeScale.invert(0);
+        const viewEndTime = timeScale.invert(width);
+        const viewport = new TimeRange(viewBeginTime, viewEndTime);
+
+        // Style of the brush area
+        let brushStyle = style ? style : {fill: "steelblue"};
+        brushStyle.cursor = "move";
+
+        if (!viewport.disjoint(timeRange)) {
+            const range = timeRange.intersection(viewport);
+            const begin = range.begin();
+            const end = range.end();
+            const [ x, y ] = [timeScale(begin), 0];
+            const endPos = timeScale(end);
+            let width = endPos - x;
+            if (width < 1) {
+                width = 1;
+            }
+
+            const bounds = {x, y, width, height};
+
+            return (
+                <rect
+                    {...bounds}
+                    style={brushStyle}
+                    pointerEvents="all"
+                    onMouseDown={this.handleBrushMouseDown}
+                    onMouseUp={this.handleMouseUp}
+                    clipPath={this.props.clipPathURL} />
+            );
         }
     },
 
-    shouldComponentUpdate() {
-        return false;
+    renderHandles() {
+        const { timeRange, timeScale, width, height, style } = this.props;
+
+        // Viewport bounds
+        const viewBeginTime = timeScale.invert(0);
+        const viewEndTime = timeScale.invert(width);
+        const viewport = new TimeRange(viewBeginTime, viewEndTime);
+
+        // Style of the brush area
+        let handleStyle = {fill: "green", opacity: 0.1, cursor: "ew-resize"};
+
+        if (!viewport.disjoint(timeRange)) {
+            const range = timeRange.intersection(viewport);
+            const [ begin, end ] = range.toJSON();
+            const [ x, y ] = [timeScale(begin), 0];
+            const endPos = timeScale(end);
+            
+            let width = endPos - x;
+            if (width < 1) {
+                width = 1;
+            }
+
+            const handleWidth = 6;
+
+            const boundsHandleLeft = {x: x - handleWidth/2, y, width: handleWidth, height};
+            const boundsHandleRight = {x: x + width - handleWidth/2, y, width: handleWidth, height};
+
+            return (
+                <g>
+                    <rect
+                        {...boundsHandleLeft}
+                        style={handleStyle}
+                        pointerEvents="all"
+                        onMouseDown={this.handleLeftHandleMouseDown}
+                        onMouseUp={this.handleMouseUp} />
+                    <rect
+                        {...boundsHandleRight}
+                        style={handleStyle}
+                        pointerEvents="all"
+                        onMouseDown={this.handleRightHandleMouseDown}
+                        onMouseUp={this.handleMouseUp} />
+                </g>
+            );
+        }
     },
 
     render() {
-        return <g ref="group"/>;
+        return (
+            <g onMouseMove={this.handleMouseMove}>
+                {this.renderOverlay()}
+                {this.renderBrush()}
+                {this.renderHandles()}
+            </g>
+        );
     }
 });
