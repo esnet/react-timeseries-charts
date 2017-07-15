17b471e0a58541e9a1a95ecefa6fb0cc91837332
Peter Murphy <peter71@gmail.com>
2017-07-15 02:58:57 -0700

    Test

---
+++ src/website/examples/climate/Index.js
@@ -0,0 +1,237 @@
+/**
+ *  Copyright (c) 2016-2017, The Regents of the University of California,
+ *  through Lawrence Berkeley National Laboratory (subject to receipt
+ *  of any required approvals from the U.S. Dept. of Energy).
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint max-len:0 */
+
+import React from "react";
+import _ from "underscore";
+
+// Pond
+import { TimeSeries } from "pondjs";
+
+// Imports from the charts library
+import Baseline from "../../../components/Baseline";
+import ChartContainer from "../../../components/ChartContainer";
+import ChartRow from "../../../components/ChartRow";
+import Charts from "../../../components/Charts";
+import YAxis from "../../../components/YAxis";
+import LineChart from "../../../components/LineChart";
+import Resizable from "../../../components/Resizable";
+import ScatterChart from "../../../components/ScatterChart";
+import EventMarker from "../../../components/EventMarker";
+
+import styler from "../../../js/styler";
+
+import climate_docs from "./climate_docs.md";
+import climate_thumbnail from "./climate_thumbnail.png";
+
+// Data
+const temperatures = require("./climate_data");
+
+const points = [];
+_.each(temperatures, val => {
+    const index = `${val.year}`;
+    const temperature = val.value;
+    const fiveyear = val.fiveyr;
+    points.push([index, temperature, fiveyear]);
+});
+
+const temperatureSeries = new TimeSeries({
+    name: "temperature anomoly",
+    columns: ["index", "temperature", "five_year"],
+    points
+});
+
+//
+// Styles
+//
+
+const baselineStyle = {
+    line: {
+        stroke: "gray",
+        strokeWidth: 1
+    }
+};
+
+const style = styler([
+    { key: "temperature", color: "#ccc", width: 1 },
+    { key: "five_year", color: "black", width: 2 }
+]);
+
+const climate = React.createClass({
+    getInitialState() {
+        return {
+            tracker: null,
+            trackerValue: "-- °C",
+            trackerEvent: null,
+            markerMode: "flag"
+        };
+    },
+    handleTrackerChanged(t) {
+        if (t) {
+            const e = temperatureSeries.atTime(t);
+            const eventTime = new Date(
+                e.begin().getTime() + (e.end().getTime() - e.begin().getTime()) / 2
+            );
+            const eventValue = e.get("temperature");
+            const v = `${eventValue > 0 ? "+" : ""}${eventValue}°C`;
+            this.setState({ tracker: eventTime, trackerValue: v, trackerEvent: e });
+        } else {
+            this.setState({ tracker: null, trackerValue: null, trackerEvent: null });
+        }
+    },
+
+    renderMarker() {
+        if (!this.state.tracker) {
+            return <g />;
+        }
+        if (this.state.markerMode === "flag") {
+            return (
+                <EventMarker
+                    type="flag"
+                    axis="axis"
+                    event={this.state.trackerEvent}
+                    column="temperature"
+                    info={[{ label: "Anomaly", value: this.state.trackerValue }]}
+                    infoTimeFormat="%Y"
+                    infoWidth={120}
+                    markerRadius={2}
+                    markerStyle={{ fill: "black" }}
+                />
+            );
+        } else {
+            return (
+                <EventMarker
+                    type="point"
+                    axis="axis"
+                    event={this.state.trackerEvent}
+                    column="temperature"
+                    markerLabel={this.state.trackerValue}
+                    markerLabelAlign="left"
+                    markerLabelStyle={{ fill: "#2db3d1", stroke: "white" }}
+                    markerRadius={3}
+                    markerStyle={{ fill: "#2db3d1" }}
+                />
+            );
+        }
+    },
+
+    renderChart() {
+        const min = -0.5;
+        const max = 1.0;
+
+        const axisStyle = {
+            labels: {
+                labelColor: "grey", // Default label color
+                labelWeight: 100,
+                labelSize: 11
+            },
+            axis: {
+                axisColor: "grey",
+                axisWidth: 1
+            }
+        };
+
+        return (
+            <ChartContainer
+                timeRange={temperatureSeries.range()}
+                timeAxisStyle={axisStyle}
+                onTrackerChanged={this.handleTrackerChanged}
+            >
+                <ChartRow height="300">
+                    <YAxis
+                        id="axis"
+                        label="Temperature Anomaly (°C)"
+                        transition={300}
+                        style={axisStyle}
+                        labelOffset={0}
+                        min={min}
+                        max={max}
+                        format=",.1f"
+                        width="60"
+                        type="linear"
+                    />
+                    <Charts>
+                        <LineChart
+                            axis="axis"
+                            series={temperatureSeries}
+                            columns={["temperature"]}
+                            style={style}
+                        />
+                        <ScatterChart
+                            axis="axis"
+                            series={temperatureSeries}
+                            columns={["temperature"]}
+                            style={style}
+                        />
+                        <LineChart
+                            axis="axis"
+                            series={temperatureSeries}
+                            columns={["five_year"]}
+                            style={style}
+                            interpolation="curveBasis"
+                        />
+                        <Baseline
+                            axis="axis"
+                            value={0.0}
+                            label="1951-1980 average"
+                            style={baselineStyle}
+                        />
+                        {this.renderMarker()}
+                    </Charts>
+                </ChartRow>
+            </ChartContainer>
+        );
+    },
+    render() {
+        const linkStyle = {
+            fontWeight: 600,
+            color: "grey",
+            cursor: "default"
+        };
+
+        const linkStyleActive = {
+            color: "steelblue",
+            cursor: "pointer"
+        };
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12" style={{ fontSize: 14, color: "#777" }}>
+                        <span
+                            style={this.state.markerMode !== "point" ? linkStyleActive : linkStyle}
+                            onClick={() => this.setState({ markerMode: "point" })}
+                        >
+                            Point marker
+                        </span>
+                        <span> | </span>
+                        <span
+                            style={this.state.markerMode !== "flag" ? linkStyleActive : linkStyle}
+                            onClick={() => this.setState({ markerMode: "flag" })}
+                        >
+                            Flag marker
+                        </span>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-md-12">
+                        <Resizable>
+                            {this.renderChart()}
+                        </Resizable>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+// Export example
+export default { climate, climate_docs, climate_thumbnail };
---
+++ src/website/examples/weather/Index.js
@@ -0,0 +1,319 @@
+/**
+ *  Copyright (c) 2015, The Regents of the University of California,
+ *  through Lawrence Berkeley National Laboratory (subject to receipt
+ *  of any required approvals from the U.S. Dept. of Energy).
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree.
+ */
+
+/* eslint max-len:0 */
+
+import React from "react";
+import _ from "underscore";
+import Moment from "moment";
+
+// Pond
+import { TimeSeries } from "pondjs";
+
+// Imports from the charts library
+import ChartContainer from "../../../components/ChartContainer";
+import ChartRow from "../../../components/ChartRow";
+import Charts from "../../../components/Charts";
+import YAxis from "../../../components/YAxis";
+import AreaChart from "../../../components/AreaChart";
+import LineChart from "../../../components/LineChart";
+import ScatterChart from "../../../components/ScatterChart";
+import Resizable from "../../../components/Resizable";
+import styler from "../../../js/styler";
+
+// Weather data
+import weatherJSON from "./weather.json";
+
+import weather_docs from "./weather_docs.md";
+import weather_thumbnail from "./weather_thumbnail.png";
+
+//
+// Read in the weather data
+//
+
+const temperaturePoints = [];
+const pressurePoints = [];
+const windPoints = [];
+const gustPoints = [];
+const rainPoints = [];
+const rainAccumPoints = [];
+_.each(weatherJSON, readings => {
+    const time = new Moment(readings.Time).toDate().getTime();
+    const tempReading = readings.TemperatureF;
+    const pressureReading = readings["PressureIn"];
+    const windReading = readings["WindSpeedMPH"] === "Calm" ? 0 : readings["WindSpeedMPH"];
+    const gustReading = readings["WindSpeedGustMPH"];
+    const rainReading = readings["HourlyPrecipIn"] === "N/A" ? 0 : readings["HourlyPrecipIn"];
+    const rainAccumReading = readings["dailyrainin"];
+
+    temperaturePoints.push([time, tempReading]);
+    pressurePoints.push([time, pressureReading]);
+
+    // Somewhat fake the wind speed...
+    windPoints.push([time, windReading * 5]);
+    if (gustReading !== "-" && gustReading !== 0) {
+        gustPoints.push([time, gustReading * 5 + Math.random() * 2.5 - 2.5, gustReading / 3]);
+    }
+    rainPoints.push([time, rainReading]);
+    rainAccumPoints.push([time, rainAccumReading]);
+});
+
+//
+// Timeseries
+//
+
+const tempSeries = new TimeSeries({
+    name: "Temperature",
+    columns: ["time", "temp"],
+    points: temperaturePoints
+});
+const pressureSeries = new TimeSeries({
+    name: "Pressure",
+    columns: ["time", "pressure"],
+    points: pressurePoints
+});
+const windSeries = new TimeSeries({
+    name: "Wind",
+    columns: ["time", "wind"],
+    points: windPoints
+});
+const gustSeries = new TimeSeries({
+    name: "Gust",
+    columns: ["time", "gust", "radius"],
+    points: gustPoints
+});
+const rainSeries = new TimeSeries({
+    name: "Rain",
+    columns: ["time", "rain"],
+    points: rainPoints
+});
+const rainAccumSeries = new TimeSeries({
+    name: "Rain Accum",
+    columns: ["time", "rainAccum"],
+    points: rainAccumPoints
+});
+
+//
+// Color scheme
+//
+
+const scheme = {
+    temp: "#CA4040",
+    pressure: "#9467bd",
+    wind: "#987951",
+    gust: "#CC862A",
+    rain: "#C3CBD4",
+    rainAccum: "#000"
+};
+
+const style = styler([
+    { key: "temp", color: "#CA4040" },
+    { key: "pressure", color: "#9467bd" },
+    { key: "wind", color: "#987951" },
+    { key: "gust", color: "#CC862A" },
+    { key: "rain", color: "#C3CBD4" },
+    { key: "rainAccum", color: "#333" }
+]);
+
+const linkStyle = {
+    fontWeight: 600,
+    color: "grey",
+    cursor: "default"
+};
+
+const linkStyleActive = {
+    color: "steelblue",
+    cursor: "pointer"
+};
+
+//
+// Render weather charts
+//
+
+const weather = React.createClass({
+    getInitialState() {
+        return {
+            tracker: null,
+            mode: "local"
+        };
+    },
+    render() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col-md-12" style={{ fontSize: 14, color: "#777" }}>
+                        <span
+                            style={this.state.mode === "local" ? linkStyleActive : linkStyle}
+                            onClick={() => this.setState({ mode: "utc" })}
+                        >
+                            UTC
+                        </span>
+                        <span> | </span>
+                        <span
+                            style={this.state.mode === "utc" ? linkStyleActive : linkStyle}
+                            onClick={() => this.setState({ mode: "local" })}
+                        >
+                            Local
+                        </span>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="row">
+                    <div className="col-md-12">
+                        <Resizable>
+                            <ChartContainer
+                                utc={this.state.mode === "utc"}
+                                timeRange={tempSeries.timerange()}
+                                showGrid={true}
+                                showGridPosition="under"
+                                trackerPosition={this.state.tracker}
+                                trackerTimeFormat="%X"
+                                onTrackerChanged={tracker => this.setState({ tracker })}
+                            >
+
+                                <ChartRow height="150">
+                                    <YAxis
+                                        id="temp"
+                                        label="Temperature (°F)"
+                                        labelOffset={-5}
+                                        style={style.axisStyle("temp")}
+                                        min={50}
+                                        max={70}
+                                        width="80"
+                                        type="linear"
+                                        format=",.1f"
+                                    />
+                                    <Charts>
+                                        <LineChart
+                                            axis="temp"
+                                            series={tempSeries}
+                                            columns={["temp"]}
+                                            style={style}
+                                        />
+                                        <LineChart
+                                            axis="pressure"
+                                            series={pressureSeries}
+                                            columns={["pressure"]}
+                                            style={style}
+                                        />
+                                    </Charts>
+                                    <YAxis
+                                        id="pressure"
+                                        label="Pressure (in)"
+                                        labelOffset={5}
+                                        style={style.axisStyle("pressure")}
+                                        min={29.5}
+                                        max={30.0}
+                                        width="80"
+                                        type="linear"
+                                        format=",.1f"
+                                    />
+
+                                </ChartRow>
+
+                                <ChartRow height="150">
+                                    <YAxis
+                                        id="wind-gust"
+                                        label="Wind gust (mph)"
+                                        labelOffset={-5}
+                                        style={style.axisStyle("gust")}
+                                        min={0}
+                                        max={50}
+                                        width="80"
+                                        type="linear"
+                                        format=",.1f"
+                                    />
+
+                                    <Charts>
+                                        <LineChart
+                                            axis="wind"
+                                            series={windSeries}
+                                            columns={["wind"]}
+                                            interpolation="curveStepBefore"
+                                            style={style}
+                                        />
+                                        <ScatterChart
+                                            axis="wind-gust"
+                                            series={gustSeries}
+                                            columns={["gust"]}
+                                            style={style}
+                                            radius={event => {
+                                                return event.get("radius");
+                                            }}
+                                        />
+                                    </Charts>
+
+                                    <YAxis
+                                        id="wind"
+                                        label="Wind (mph)"
+                                        labelOffset={5}
+                                        style={{ labelColor: scheme.wind }}
+                                        min={0}
+                                        max={50}
+                                        width="80"
+                                        type="linear"
+                                        format=",.1f"
+                                    />
+                                </ChartRow>
+
+                                <ChartRow height="150">
+                                    <YAxis
+                                        id="total-rain"
+                                        label="Total Precipitation (in)"
+                                        style={style.axisStyle("rainAccum")}
+                                        labelOffset={-5}
+                                        min={0}
+                                        max={rainAccumSeries.max("rainAccum")}
+                                        width="80"
+                                        type="linear"
+                                        format=",.2f"
+                                    />
+                                    <Charts>
+                                        <AreaChart
+                                            axis="rain"
+                                            series={rainSeries}
+                                            columns={{ up: ["rain"] }}
+                                            style={style}
+                                            interpolation="curveBasis"
+                                            fillOpacity={0.4}
+                                        />
+                                        <LineChart
+                                            axis="total-rain"
+                                            series={rainAccumSeries}
+                                            columns={["rainAccum"]}
+                                            style={style}
+                                        />
+                                    </Charts>
+                                    <YAxis
+                                        id="rain"
+                                        label="Precipitation (in)"
+                                        labelOffset={5}
+                                        style={style.axisStyle("rain")}
+                                        min={0}
+                                        max={rainSeries.max("rain")}
+                                        width="80"
+                                        type="linear"
+                                        format=",.2f"
+                                    />
+                                </ChartRow>
+
+                            </ChartContainer>
+                        </Resizable>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+// Export example
+export default { weather, weather_docs, weather_thumbnail };
