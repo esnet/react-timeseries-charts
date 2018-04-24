ff256c172276b5e58019a1ac2bc0b9839dfd0961
sartaj10 <ssbaveja@es.net>
2018-04-24 16:27:13 -0700

    fix 3

--- src/components/ChartContainer.js
+++ src/components/ChartContainer.js
@@ -15,6 +15,8 @@ import PropTypes from "prop-types";
 import { scaleTime, scaleUtc } from "d3-scale";
 import { TimeRange } from "pondjs";
 
+import { areComponentsEqual } from "react-hot-loader";
+
 import Brush from "./Brush";
 import MultiBrush from "./MultiBrush";
 import ChartRow from "./ChartRow";
@@ -130,13 +132,27 @@ export default class ChartContainer extends React.Component {
 
         React.Children.forEach(this.props.children, childRow => {
             console.log("here ", childRow.type, ChartRow);
+            const element = <ChartRow />;
+            if (areComponentsEqual(childRow.type, ChartRow)) {
+                console.log("areComponentsEqual works");
+            }
+
+            if (childRow.type === React.createElement(ChartRow).type) {
+                console.log("createElement works");
+            }
+
+            const ChartRowType = <ChartRow />.type;
+            if (childRow.type === ChartRowType) {
+                console.log("chartRowType works");
+            }
+
             if (childRow.type === ChartRow) {
                 //
                 // Within this row, count the number of columns that will be
                 // left and right of the Charts tag, as well as the total number
                 // of Charts tags for error handling
                 //
-
+                console.log("here inside");
                 let countLeft = 0;
                 let countCharts = 0;
 
